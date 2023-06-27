83d140e8817671e349e1dac0eccbdb058eb2c1b1
Alexander Semyahin <alexsander200121@gmail.com>
2023-06-28 00:25:22 +0300

    Add usePosts in usePost

--- src/hooks/usePost.jsx
+++ src/hooks/usePost.jsx
@@ -9,3 +9,12 @@ export const useSortedPosts = (posts, sort) => {
     return sortedPosts;
 }
 
+export const usePosts = (posts, sort, query) => {
+    const sortedPosts = useSortedPosts(posts, sort);
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return (sortedPosts.filter(post => post.title.toLowerCase().includes(query)))
+    }, [query, sortedPosts])
+
+    return (sortedAndSearchedPosts);
+}
