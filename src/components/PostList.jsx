1d59cba1f202f647fd2581ec8ae69495b1fe8131
Alexander Semyahin <alexsander200121@gmail.com>
2023-06-27 23:44:46 +0300

    Using TransitionGroup. Create animation for creature and removal posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { PostItem } from "./PostItem";
+import { TransitionGroup } from "react-transition-group";
+import {CSSTransition} from "react-transition-group";
 
 export const PostList = ({post, title, remove}) => {
     if (!post.length)
@@ -10,9 +12,17 @@ export const PostList = ({post, title, remove}) => {
         return (
             <div>
                 <h1 style={{ textAlign: "center", color: "green" }}>{title}</h1>
-                {post.map((post, index) =>
-                    <PostItem number={index + 1} post={post} key={post.id} remove={remove}/>
-                )}
+                <TransitionGroup>
+                    {post.map((post, index) =>
+                        <CSSTransition
+                            key={post.id}
+                            timeout={500}
+                            classNames="post"
+                        >
+                        <PostItem number={index + 1} post={post} key={post.id} remove={remove} />
+                        </CSSTransition>
+                    )}
+                </TransitionGroup>
             </div>
         )
 }
