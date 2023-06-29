7d24f2bd82c85eb491edcc241646f943d56a7215
Alexander Semyahin <alexsander200121@gmail.com>
2023-06-30 00:12:44 +0300

    Using UseEffectHook. Immitation downloading of data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css'
 import { PostList } from './components/PostList';
 import { PostForm } from './components/PostForm';
@@ -6,42 +6,54 @@ import { PostFilter } from './components/PostFilter';
 import { MyModal } from './components/UI/MyModal/MyModal';
 import { MyButton } from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePost';
-import axios from 'axios';
+import PostService from './API/PostService';
 
 function App() {
   const [posts, setPosts] = useState([])
 
-    const [filter, setFilter] = useState({sort: '', query: ''})
-    const [modal, setModal] = useState(false)
-    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  const [filter, setFilter] = useState({ sort: '', query: '' })
+  const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  const [isPostsLoading, setIsPostsLoading] = useState(false)
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   const createPost = (newPost) => {
-      setPosts([...posts, newPost]);
-      setModal(false);
+    setPosts([...posts, newPost]);
+    setModal(false);
   }
 
   async function fetchPosts() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    setPosts(response.data);
+    setIsPostsLoading(true);
+    setTimeout(async () => {
+          const posts = await PostService.getALL();
+          setPosts(posts);
+          setIsPostsLoading(false);
+    }, 1000)
+    
   }
 
   const removePost = (post) => {
-      setPosts(posts.filter(item => item.id !== post.id))
+    setPosts(posts.filter(item => item.id !== post.id))
   }
 
   return (
-    <div className="App">
-      <button onClick={fetchPosts}>GET POSTS</button>
-      <MyButton style={{marginTop: "30px"}} onClick={() => setModal(true)}>Создать пользователя</MyButton>
+    <div className="App" fetchPosts={fetchPosts}>
+      <MyButton style={{ marginTop: "30px" }} onClick={() => setModal(true)}>Создать пользователя</MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
-      <hr style={{margin: "15px 0"}}/>
+      <hr style={{ margin: "15px 0" }} />
       <PostFilter
         filter={filter}
         setFilter={setFilter}
       />
-      <PostList post={sortedAndSearchedPosts} title="Список постов JS" remove={removePost}/>
+      {isPostsLoading ?
+      <h1 style={{textAlign: "center"}}>Загрузка данных...</h1> :
+      <PostList post={sortedAndSearchedPosts} title="Список постов JS" remove={removePost} />
+      }
     </div>
   );
 }
